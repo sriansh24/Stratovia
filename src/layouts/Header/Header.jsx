37c2d61d7d981ed2485f21340f6cc8241d874eee
sriansh24 <pattanaiksrianshu@gmail.com>
2026-03-02 20:26:59 +0530

    Toggle button is fixed now for mobile and tablet screens.

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { Menu } from "lucide-react";
 import logo from "../../assets/img/logo/stratovia-logo5.webp";
-// import logo from "../../assets/img/logo/stratovia-logo2.png";
-import "../../assets/css/GlobalCss/styles.css"
+import "../../assets/css/GlobalCss/styles.css";
 
 function MainLayoutHeader() {
+  const [expanded, setExpanded] = useState(false);
   return (
     <>
       <Navbar
@@ -14,26 +15,31 @@ function MainLayoutHeader() {
         data-bs-theme="dark"
         expand="lg"
       >
-        <Container fluid className="d-flex align-items-center flex-nowrap mx-md-3 px-md-4">
+        <Container
+          fluid
+          className="d-flex align-items-center flex-nowrap mx-md-3 px-md-4"
+        >
           <Navbar.Brand href="/stratovia/home" className="me-4">
-            <img
-              src={logo}
-              className="navbar-logo"
-              alt="Stratovia Logo"
-            />
-            {/* <p><i>STRATOVIA</i></p> */}
+            <img src={logo} className="navbar-logo" alt="Stratovia Logo" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="main-navbar-nav" />
-          <Navbar.Collapse id="main-navbar-nav">
+          <button
+            className="custom-toggler"
+            onClick={() => setExpanded(!expanded)}
+          >
+            <Menu />
+          </button>
+          <div className={`custom-collapse ${expanded ? "open" : ""}`}>
             <Nav className="ms-auto align-items-center gap-lg-4">
               <Nav.Link href="/stratovia/home">Home</Nav.Link>
               <Nav.Link href="/stratovia/blog">Blog</Nav.Link>
               <Nav.Link href="/stratovia/contact-us">Contact Us</Nav.Link>
               <Nav.Link href="/stratovia/help">Help</Nav.Link>
-              <Nav.Link href="/stratovia/stratovia-airo">StratoviaAiro</Nav.Link>
+              <Nav.Link href="/stratovia/stratovia-airo">
+                StratoviaAiro
+              </Nav.Link>
               <Nav.Link href="/stratovia/pricing">Pricing</Nav.Link>
             </Nav>
-          </Navbar.Collapse>
+          </div>
         </Container>
       </Navbar>
     </>
